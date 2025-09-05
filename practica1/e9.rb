b01a9c2881f54ee38bbc004ef50f9c545d9f3bb9
MateoCarusotti <mateocarusotti@gmail.com>
2025-09-05 13:10:51 -0300

    act e9

---
+++ practica1/e9.rb
@@ -0,0 +1,16 @@
+=begin
+Dada una cadena cualquiera, y utilizando los métodos que provee la clase String, realizá
+las siguientes operaciones sobre dicha cadena, implementando métodos que funcionen de la
+siguiente forma:
+string_reverso: retorna el string con los caracteres en orden inverso.
+string_sin_espacio: elimina los espacios en blanco que contenga.
+string_a_arreglo_ascii: retorna un arreglo con cada uno de los caracteres conver-
+tidos a su correspondiente valor ASCII.
+string_remplaza_vocal: cambia las vocales por números:
+– "a" o "A" por "4",
+– "e" o "E" por "3",
+– "i" o "I" por "1",
+– "o" u "O" por "0",
+– y "u" o "U" por "6"
+
+=end
